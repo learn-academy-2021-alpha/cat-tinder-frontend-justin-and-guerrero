2097ea71c94455cae24029c5de0a0f9fe04d05bd
justinhilario <justinhilario@gmail.com>
2021-03-30 11:54:44 -0700

    router switch

--- src/App.js
+++ src/App.js
@@ -9,14 +9,38 @@ import SuperheroEdit from './pages/SuperheroEdit';
 import SuperheroIndex from './pages/SuperheroIndex';
 import SuperheroNew from './pages/SuperheroNew';
 import SuperheroShow from './pages/SuperheroShow';
+import mockSuperheroes from './mockSuperheroes';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      
-      
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      superheroes: mockSuperheroes
+    }
+  }
+  render (){
+    return (
+      <>
+        <Router>
+          < Header />
+            <Switch>
+              <Route exact path= "/" component= { Home }/> 
+              <Route path="/superheroedit/:id" component={ SuperheroEdit }/>
+              <Route path="/superheroindex" component={ SuperheroIndex } />
+              <Route path="/superheronew" component={ SuperheroNew }/>
+              <Route path="/superheroshow/:id" component={ SuperheroShow }  />
+              <Route component= { NotFound } />
+            </Switch>
+          < Footer />
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/mockSuperheroes.js
@@ -0,0 +1,33 @@
+let superheroes = [
+    {
+      id: 1,
+      name: "Batman",
+      age: 40,
+      enjoys: "Long walks on the beach, staring at the sunset"
+    },
+    {
+      id: 2,
+      name: "Iron-Man",
+      age: 48,
+      enjoys: "Messing with Spiderman in the afterlife"
+    },
+    {
+      id: 3,
+      name: "Spiderman",
+      age: 18,
+      enjoys: "Getting movie reboots, Mary Jane"
+    },
+    {
+      id: 4,
+      name: "Superman",
+      age: 22,
+      enjoys: "Blogging"
+    },
+    {
+      id: 5,
+      name: "Joker",
+      age: 35,
+      enjoys: "HMU to find out :)"
+    }
+  ]
+  export default superheroes
